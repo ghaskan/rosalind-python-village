f436ca910dbde4d5e7fb0f837f83a0741d8c6c86
ghaskan <ghaskan@gmail.com>
2016-04-17 09:28:24 +0100

    Create intro-dicts.py

---
+++ intro-dicts.py
@@ -0,0 +1,18 @@
+input = open('C:\\Users\\win-7\\Downloads\\rosalind_ini6.txt', 'r')
+output = open('C:\\Users\\win-7\\Desktop\\derp.txt','w')
+
+data = input.read()
+freq = {}
+
+for word in data.split(' '):
+        word = word.rstrip('\n')
+        word = word.rstrip('\r')
+        freq[word] = freq.get(word,0) + 1
+
+for value in freq:
+    output.write(str(value) + ' ' + str(freq[value]) + '\n')
+
+print(freq)
+
+input.close()
+output.close()
